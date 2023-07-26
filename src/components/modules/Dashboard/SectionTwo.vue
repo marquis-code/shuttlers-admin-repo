c07409fcff761d939f7ecf26d2eeef89d1ebcbde
Marquis Abah <marquis.abah@shuttlers.ng>
2023-07-26 08:26:35 +0100

    setup trip and trip rating charts and date pickers

--- src/components/modules/Dashboard/SectionTwo.vue
+++ src/components/modules/Dashboard/SectionTwo.vue
@@ -1,17 +1,41 @@
 <template>
-	<section class="card h-[400px]">
+	<section class="stat_card">
 		<div class="border-b">
-			<h3 class="font-medium py-4 px-6">
+			<h3 class="font-medium pb-3.5 px-6">
 				Trips
 			</h3>
+		</div>
+		<div class="p-5">
+			<Bar style="width: 100%" class="h-[400px]" :options="chartOptions" :data="chartData" />
 		</div>
 	</section>
 </template>
 
 <script setup lang="ts">
+import { Bar } from 'vue-chartjs'
+import { ref, computed } from 'vue'
+const chartData = ref({
+	labels: ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec'],
+	datasets: [
+		{
+			label: 'My First Dataset',
+			data: [12, 0, 81, 5, 14, 27, 11, 0, 0, 0, 0, 0],
+			backgroundColor: [
+				'rgba(153, 102, 255, 0.2)'
+			]
+		}
+	]
+})
 
+const chartOptions = computed(() => {
+	return {
+		responsive: true, scales: {
+			y: {
+				beginAtZero: true
+			}
+		}
+	}
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
