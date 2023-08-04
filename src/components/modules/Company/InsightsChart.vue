f0aa51d44b09b5482e709fb704e4e33b1d523ef5
Marquis Abah <marquis.abah@shuttlers.ng>
2023-08-04 08:23:26 +0100

    rework company drivers

---
+++ src/components/modules/Company/InsightsChart.vue
@@ -0,0 +1,25 @@
+<template>
+	<section class="stat_card">
+		<div class="border-b">
+			<h3 class="font-medium pb-3.5 px-6">
+				Companies Growth
+			</h3>
+		</div>
+		<div class="p-5">
+			<Bar style="width: 100%" class="h-[400px]" :options="chartOptions" :data="chartData" />
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	chartOptions: {
+		type: Object,
+		required: true
+	},
+	chartData: {
+		type: Object,
+		required: true
+	}
+})
+</script>
