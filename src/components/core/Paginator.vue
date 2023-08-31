bd9b9527e5c3a02f7481796230770e04eba6e97c
kromate <akpananthony33@gmail.com>
2023-08-31 15:14:03 +0100

    add pagination and redirect page

---
+++ src/components/core/Paginator.vue
@@ -0,0 +1,43 @@
+<template>
+	<footer class="flex items-center justify-between w-full mb-12">
+		<span v-if="!loading" class="text-sm">
+			Showing {{ currentPage }} of {{ totalPages }}
+		</span>
+		<span v-else />
+
+		<section class="flex gap-4">
+			<button v-if="currentPage !==1" class="btn-outline " @click="emit('first')">
+				First
+			</button>
+			<button class="btn-outline " :disabled="currentPage === 1" @click="emit('prev')">
+				Previous
+			</button>
+			<button class="btn-outline" :disabled="currentPage === totalPages" @click="emit('next')">
+				Next
+			</button>
+		</section>
+	</footer>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['next', 'prev', 'first'])
+defineProps({
+    loading: {
+        type: Boolean,
+        required: true
+    },
+    currentPage: {
+        type: Number,
+        required: true
+    },
+    totalPages: {
+        type: Number,
+        required: true
+	}
+
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/redirect.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="h-screen p-5 center">
+		<img src="@/assets/images/core/logo.svg" alt="logo" class="w-96">
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
