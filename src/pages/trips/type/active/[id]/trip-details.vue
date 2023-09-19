f3241e1f46121e80c68fc30af3dd7a933fbf7473
Marquis Abah <marquis.abah@shuttlers.ng>
2023-09-19 07:19:08 +0100

    fix build errors

---
+++ src/pages/trips/type/active/[id]/trip-details.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1>Trip details here</h1>
+	</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+	layout: 'dashboard',
+	middleware: ['is-authenticated']
+})
+
+</script>
+
+<style scoped>
+
+</style>
