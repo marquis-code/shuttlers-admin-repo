a10ed2e39d43cde8aea01ea87507de8dd29595f4
Marquis Abah <marquis.abah@shuttlers.ng>
2023-10-25 08:34:28 +0100

    setup routes managenment

--- src/api_factory/modules/routes.ts
+++ src/api_factory/modules/routes.ts
@@ -13,7 +13,24 @@ export const routes_api = {
 	},
 	$_get_main_routes: (meta:TMetaObject, filterData?: Record<string, Ref>) => {
 		const queryParams = useTableFilter(filterData)
-		const url = `routes?fields[route]=id,pickup,destination,status,route_code,corporate_id,is_exclusive,visibility&sort[pickup]=asc&related=&limit=${meta.page_size.value}&page=${meta.page.value}&metadata=true&${queryParams}`
+		const url = `/routes?fields[route]=id,pickup,destination,status,route_code,corporate_id,is_exclusive,visibility&sort[pickup]=asc&related=&limit=${meta.page_size.value}&page=${meta.page.value}&metadata=true&${queryParams}`
+		return GATEWAY_ENDPOINT_WITH_AUTH.get(url)
+	},
+	$_get_route_by_id: (id:string) => {
+		const url = `/routes/${id}`
+		return GATEWAY_ENDPOINT_WITH_AUTH.get(url)
+	},
+	$_get_route_busstops_by_id: (id:number) => {
+		const url = `/routes/${id}/busstops`
+		return GATEWAY_ENDPOINT_WITH_AUTH.get(url)
+	},
+	$_get_route_itineraries_by_id: (id:number) => {
+		const url = `/routes/${id}/itineraries?itinerary_only=1`
+		return GATEWAY_ENDPOINT_WITH_AUTH.get(url)
+	},
+	$_get_route_trips_by_id: (id:number, meta:TMetaObject, filterData?: Record<string, Ref>) => {
+		const queryParams = useTableFilter(filterData)
+		const url = `/routes/${id}/trips?${queryParams}&limit=${meta.page_size.value}&page=${meta.page.value}`
 		return GATEWAY_ENDPOINT_WITH_AUTH.get(url)
 	}
 }
--- src/components/modules/Dashboard/SectionFour.vue
+++ src/components/modules/Dashboard/SectionFour.vue
@@ -1,54 +1,53 @@
 <template>
-	<section class="items-start space-y-6 lg:flex lg:space-x-10 lg:space-y-0">
+	<section class="space-y-6 lg:flex justify-start items-start lg:space-x-10 lg:space-y-0">
 		<section :class="[loadingTransactions ? 'h-[400px]' : '']" class="rounded-lg lg:w-6/12 stat-card">
 			<div class="">
 				<h3 class="px-6 py-4 font-medium">
 					Last 5 Transactions
 				</h3>
 			</div>
-			<div class="relative flex flex-col w-full min-w-0 break-words h-[495px] overflow-y-auto">
-				<div v-if="!loadingTransactions" class="block w-full overflow-x-auto ">
-					<Table :headers="transactionHeaders" :table-data="computedTransactionsList" :loading="loadingTransactions">
-						<template #item="{ item }">
-							<span v-if="item.created_at" class="flex items-center gap-4 py-3">
-								<span class="text-sm">{{ item.data.created_at }}</span>
-							</span>
-							<span v-if="item.title" class="flex items-center gap-4 py-3">
-								<span class="text-sm">{{ item.data.title }}</span>
-							</span>
-							<span v-if="item.user" class="flex items-center gap-4 py-3">
-								<span class="text-sm">{{ item.data.user.fname }} {{ item.data.user.lname }}</span>
-							</span>
-							<span v-if="item.amount" class="flex items-center gap-4 py-3">
-								<span class="text-sm">{{ convertToCurrency(item.data.amount) }}</span>
-							</span>
-						</template>
-					</Table>
-				</div>
-				<Skeleton v-else height="300px" />
+			<div v-if="!loadingTransactions" class="block w-full overflow-x-auto ">
+				<Table :headers="transactionHeaders" :table-data="computedTransactionsList" :loading="loadingTransactions">
+					<template #item="{ item }">
+						<span v-if="item.created_at" class="flex items-center gap-4 py-4">
+							<span class="text-xs">{{ useDateFormat(item.data.created_at, "hh:mm, MMMM d, YYYY").value }}</span>
+						</span>
+						<div v-if="item.title" class="flex items-center gap-4 py-4 w-[200px]">
+							<span class="text-xs text-relaxed">{{ item.data.title }}</span>
+						</div>
+						<span v-if="item.user" class="flex items-center gap-4 py-4">
+							<span class="text-sm">{{ item.data.user.fname }} {{ item.data.user.lname }}</span>
+						</span>
+						<span v-if="item.amount" class="flex items-center gap-4 py-4">
+							<span :class="[item.data.type === 'debit' ? 'text-red' : 'text-shuttlersGreen']" class="text-sm">{{ convertToCurrency(item.data.amount) }}</span>
+						</span>
+					</template>
+				</Table>
 			</div>
+			<Skeleton v-else height="300px" />
 		</section>
 		<section class="lg:w-6/12 stat-card">
 			<h3 class="px-6 py-4 font-medium">
 				Last 5 User Signups
 			</h3>
-			<div v-if="!loadingSignups" class="relative flex flex-col w-full min-w-0 break-words">
-				<div class="block w-full overflow-x-auto ">
-					<Table :headers="signupHeaders" :table-data="signupList" :loading="loadingSignups">
-						<template #item="{ item }">
-							<span v-if="item.fname" class="flex items-center gap-4">
-								<span class="text-sm">{{ item.data.fname }} {{ item.data.lname }}</span>
-							</span>
-							<span v-if="item.email" class="flex items-center gap-4">
-								<span class="text-sm">{{ item.data.email }}</span>
-							</span>
-							<span v-if="item.active" class="flex items-center gap-4">
-								<span class="text-sm">{{ item.data.active == '1' ? 'Active' : 'Inactive' }}</span>
-							</span>
-						</template>
-					</Table>
-				</div>
-				<div v-if="!loadingSignups" class="flex items-end justify-end py-4 pr-3">
+			<div v-if="!loadingSignups" class="">
+				<!-- <div class="block w-full overflow-x-auto ">  -->
+				<Table :headers="signupHeaders" :table-data="signupList" :loading="loadingSignups">
+					<template #item="{ item }">
+						<span v-if="item.fname" class="flex items-center gap-4 py-6">
+							<Avatar :name="item.data.fname" bg="#B1C2D9" />
+							<span class="text-sm">{{ item.data.fname }} {{ item.data.lname }}</span>
+						</span>
+						<span v-if="item.email" class="flex items-center gap-4 py-6">
+							<span class="text-sm">{{ item.data.email }}</span>
+						</span>
+						<span v-if="item.active" class="flex items-center gap-4 py-6">
+							<span class="text-sm">{{ item.data.active == '1' ? 'Active' : 'Inactive' }}</span>
+						</span>
+					</template>
+				</Table>
+				<!-- </div> -->
+				<div class="flex items-end justify-end py-4 pr-3">
 					<NuxtLink to="/users" class="flex items-center justify-center text-xs text-blue-500 gap-x-2">
 						All Users<img class="inline" src="@/assets/icons/source/next.svg" alt="">
 					</NuxtLink>
@@ -60,6 +59,7 @@
 </template>
 
 <script setup lang="ts">
+import { useDateFormat } from '@vueuse/core'
 import { useGetRecentTransactionsList } from '@/composables/modules/transactions/fetch'
 import { useGetRecentSignupsList } from '@/composables/modules/users/fetch'
 import { convertToCurrency } from '@/composables/utils/formatter'
