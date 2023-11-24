5869b2a8ce9567cd2eba3af09230d674a3f945db
Marquis Abah <marquis.abah@shuttlers.ng>
2023-11-24 08:43:33 +0100

    work on routes functionalities

--- src/components/modals/modules/routes/DuplicateRoutes.vue
+++ src/components/modals/modules/routes/DuplicateRoutes.vue
@@ -3,22 +3,25 @@
 		modal="$atts.modal"
 		title="Duplicate Routes"
 		:no-close-btn="true"
-		class="text-center"
 	>
-		<div class="flex flex-col items-center justify-center space-y-6">
+		<div class="space-y-6">
 			<div class="field relative">
 				<label for="routeCode" class="text-[12px] text-[#6E717C] font-[500] leading-[20px]">New route code</label>
-				<input id="routeCode" v-model="routeCreationForm.route_code" placeholder="₦" autocomplete="true" type="text" class="input-field" required>
+				<input id="routeCode" v-model="routeCreationForm.new_route_code" type="text" class="input-field" required>
 			</div>
-			<div class="field relative flex items-center">
-				<label for="copy_itiberaries" class="text-[12px] text-[#6E717C] font-[500] leading-[20px]">Copy Itineraries</label>
-				<input v-model="routeCreationForm.copy_itinerary" type="checkbox" class="" name="copy_itiberaries" id="copy_itiberaries" />
+			<div class="flex items-center gap-x-2">
+				<p><label for="copy_itiberaries" class="text-[12px] text-[#6E717C] font-[500] leading-[20px]">Copy Itineraries</label></p>
+				<p><input id="copy_itiberaries" v-model="routeCreationForm.copy_itineraries" type="checkbox" class="" name="copy_itiberaries"></p>
 			</div>
 		</div>
 		<div class="flex justify-between items-center gap-x-6 mt-6">
-			<div class="w-full"><button @click="useTripsModal().closeDeductEarning()" class="text-white bg-gray-500 w-full rounded-md py-2.5">Close</button></div>
 			<div class="w-full">
-				<button :disabled="!isFormEmpty" :class="[!isFormEmpty ? 'opacity-25 cursor-not-allowed' : null]" class="font-[700] w-full py-2.5 text-white flex justify-center items-center rounded-md bg-[#000005]" @click="handleDeduction">
+				<button class="text-white bg-gray-500 w-full rounded-md py-2.5" @click="useRouteModal().closeRouteDuplicationModal()">
+					Close
+				</button>
+			</div>
+			<div class="w-full">
+				<button :disabled="!isFormEmpty" :class="[!isFormEmpty ? 'opacity-25 cursor-not-allowed' : null]" class="font-[700] w-full py-2.5 text-white flex justify-center items-center rounded-md bg-[#000005]" @click="handleRouteDuplication">
 					<span v-if="!loading" class="flex justify-center items-center gap-2.5">Duplicate Route</span>
 					<Spinner v-else />
 				</button>
@@ -28,16 +31,22 @@
 </template>
 
 <script setup lang="ts">
+import { useDuplicateRoute } from '@/composables/modules/routes/updateRoute/duplicate'
+import { useRouteModal } from '@/composables/core/modals'
+const { prePopulateRouteDuplicationForm, duplicateRoute, loading } = useDuplicateRoute()
 const isFormEmpty = computed(() => {
-	return !!(routeCreationForm.route_code)
+	return !!(routeCreationForm.new_route_code)
 })
-
 const routeCreationForm = reactive({
-    route_code: '',
-    copy_itinerary: ''
+    new_route_code: '',
+	route_id: '',
+    copy_itineraries: ''
 })
+const handleRouteDuplication = () => {
+	prePopulateRouteDuplicationForm(routeCreationForm)
+	duplicateRoute()
+}
 </script>
 
 <style>
-
 </style>
--- src/components/modals/modules/trips/TransferBooking.vue
+++ src/components/modals/modules/trips/TransferBooking.vue
@@ -5,119 +5,48 @@
 		:no-close-btn="true"
 	>
 		<section class="space-y-4">
-			<p v-if="form.selectedRoute" class="text-gray-500 text-sm">
-				Transfer bookings to {{ form?.selectedRoute?.route_code }} ({{ form?.selectedRoute?.pickup }} <span class="font-semibold">to</span> {{ form?.selectedRoute?.destination }})
-				{{ form?.selectedItinerary?.trip_time }}
+			<p v-if="Object.keys(routeObj).length" class="text-gray-500 text-sm">
+				Transfer bookings to {{ routeObj?.route_code }} ({{ routeObj?.pickup }} <span class="font-semibold">to</span> {{ routeObj?.destination }})
+				{{ itineraryObj?.trip_time }}
 			</p>
-			<div>
-				<div><RuoteSelector class="" @selected="routeSelected" /></div>
-				<p class="text-sm text-gray-500 mt-1">
-					Please select a route
-				</p>
+			<div v-if="bookingTransferActiveStep === '1'" class="space-y-6">
+				<ModulesTripsBookingTransferSectionOne
+					:booking-transfer-active-step="bookingTransferActiveStep"
+					:processing-booking="processingBooking"
+					@route-description="handleSelectedRoute"
+					@itinerary-description="handleSelectedItinerary"
+					@stepFormComplete="bookingTransferActiveStep = '2'"
+				/>
 			</div>
-			<div class="field relative">
-				<div v-if="!loadingItineraries" class="w-full">
-					<label for="itinerary" class="text-sm">Assign Itinerary</label>
-					<select id="itinerary" v-model="form.selectedItinerary"
-						class="border-red-500 w-full text-sm border outline-none py-2.5 rounded-md px-3 input-field">
-						<option value="" class="" disabled>
-							--- select ---
-						</option>
-						<option v-for="(itinerary, idx) in routeItineraries" :key="idx" :value="itinerary">
-							{{ itinerary?.trip_time }}
-						</option>
-					</select>
-				</div>
-				<Skeleton v-else height="50px" />
-			</div>
-			<div class="field relative">
-				<div v-if="!loadingVehicles" class="w-full">
-					<label for="itinerary" class="text-sm">Selected Vehicle</label>
-					<select id="itinerary" v-model="form.selectedVehicle"
-						class="border-red-500 w-full text-sm border outline-none py-2.5 rounded-md px-3 input-field">
-						<option value="" class="" disabled>
-							--- select ---
-						</option>
-						<option v-for="(item, idx) in itinerariesVehicles" :key="idx" :value="item">
-							<p class="mb-0">
-								{{ item?.vehicle?.brand }} •
-								{{ item?.vehicle?.name }} {{ item?.vehicle?.registration_number }} •
-								{{ item?.vehicle?.seats }} Seater
-							</p>
-							<p class="mb-0 pl-3">
-								Driver: {{ item?.driver?.fname }} {{ item?.driver?.lname }}
-							</p>
-						</option>
-					</select>
-				</div>
-				<Skeleton v-else height="50px" />
-			</div>
-			<div class="flex items-center gap-x-2">
-				<input id="enableTransgerBookingForTodayOnly" v-model="enableTransgerBookingForTodayOnly" :value="enableTransgerBookingForTodayOnly" name="enableTransgerBookingForTodayOnly" class="" type="checkbox">
-				<label class="pt-2">Transfer booking for only today</label>
-			</div>
-			<div v-if="!enableTransgerBookingForTodayOnly" class="space-y-4">
-				<div class="">
-					<label class="font-light text-sm">Start Date (Today)</label>
-					<input readonly :value="todaysDate" class="w-full input-field border outline-none py-2 text-xs px-3 rounded-md">
-				</div>
-				<div class="">
-					<label class="font-light text-sm">End Date</label>
-					<InputDateInput id="startDate" v-model="form.endDate" class="font-light  w-full" placeholder="Filter by date" />
-				</div>
-			</div>
-			<div class="flex justify-between items-center gap-x-10 pt-6">
-				<button type="submit" class="bg-gray-600 py-3 rounded-md text-xs text-white w-full"
-					@click="useTripsModal().closeTransferBooking()">
-					Cancel
-				</button>
-				<button :disabled="!isFormEmpty" type="submit"
-					class="btn btn-primary py-3 text-xs w-full disabled:cursor-not-allowed disabled:opacity-25">
-					<span v-if="!processingBooking" class="text-xs">Continue</span>
-					<Spinner v-else />
-				</button>
+
+			<div v-if="bookingTransferActiveStep === '2'">
+				<ModulesTripsBookingTransferSectionTwo @go-back="bookingTransferActiveStep = '1'" :table-data="tableData" :loading="loading" />
 			</div>
 		</section>
 	</Modal>
 </template>
 <script setup lang="ts">
-import { useDateFormat } from '@vueuse/core'
-import { useItinerariesByRouteId } from '@/composables/modules/routes/id'
-import { useVehiclesByItineraryId } from '@/composables/modules/routes/vehicles/fetch'
-import { useTripsModal } from '@/composables/core/modals'
-const { getVehiclesByItineraryId, loadingVehicles, itinerariesVehicles } = useVehiclesByItineraryId()
-const { routeItineraries, loading: loadingItineraries, getRouteItinerariesByRouteId } = useItinerariesByRouteId()
-const form = reactive({
-    selectedRoute: null,
-    route_itinerary_id: '',
-    selectedVehicle: null,
-    selectedItinerary: null,
-    endDate: ''
-})
-const enableTransgerBookingForTodayOnly = ref(true)
-watch(() => form.selectedRoute, (val) => {
-	if (val) {
-		getRouteItinerariesByRouteId(form?.selectedRoute?.id)
-	}
-})
+const loading = ref(false)
 const processingBooking = ref(false)
-watch(() => form.selectedItinerary, (val) => {
-   if (val) {
-    getVehiclesByItineraryId(val.id)
-   }
-})
+const bookingTransferActiveStep = ref('1')
 
-const todaysDate = computed(() => {
-   return useDateFormat(new Date(), 'YYYY-MM-DD').value
-})
+const tableData = reactive([
+	{
+		passengers_name: 'Mr Joker',
+		old_pickup: 'West End',
+		old_dropoff: 'Evercare Hospital Lekki, Bisola Durosinmi Etti Drive, Lagos, Nigeria'
+	}
+])
 
-const routeSelected = (val: any) => {
-	form.selectedRoute = val
+const routeObj = ref({})
+const itineraryObj = ref({})
+const handleSelectedRoute = (val) => {
+   routeObj.value = val
 }
-
-const isFormEmpty = computed(() => {
-    return !!(form.selectedItinerary && form.selectedRoute && form.selectedVehicle && form.endDate)
-})
+const handleSelectedItinerary = (val) => {
+	console.log(val, 'gh')
+	itineraryObj.value = val
+}
 </script>
 
 <style>
