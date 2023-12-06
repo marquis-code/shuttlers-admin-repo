11368c8d46e8310846a16331ca6a136023ec3333
Abubakri Sherif <sherif.abubakri@shuttlers.ng>
2023-12-06 12:38:27 +0100

    update on sidebar

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -6,17 +6,18 @@
 import Sidebar from './sidebarComp.vue'
 import { ADMIN_SIDEBAR_MENUS, getSidebarMenus, OPS_AND_MANAGEMENT_SIDEBAR_MENUS, TOP_LEVEL_SIDEBAR_MENUS } from '@/utils/sidebar'
 import { useUser } from '@/composables/auth/user'
-import { currentUserPermission } from '@/composables/core/permission'
+import { currentUserPermission, useUserPermission } from '@/composables/core/permission'
 import logoIcon from '@/assets/icons/src/logo.vue'
 import menuIcon from '@/assets/icons/src/menu.vue'
 import { useAuthModal } from '@/composables/core/modals'
 const { user } = useUser()
-
+const { fetchUserPermission } = useUserPermission()
+fetchUserPermission()
 const permissionModules = currentUserPermission.value
 
-const topLayerMenus = ref(getSidebarMenus(TOP_LEVEL_SIDEBAR_MENUS, permissionModules, (user.value.role || 'user')))
-const featureMenus = ref(getSidebarMenus(OPS_AND_MANAGEMENT_SIDEBAR_MENUS, permissionModules, (user.value.role || 'user')))
-const adminMenus = ref(getSidebarMenus(ADMIN_SIDEBAR_MENUS.value, permissionModules, (user.value.role || 'user')))
+const topLayerMenus = ref(getSidebarMenus(TOP_LEVEL_SIDEBAR_MENUS, currentUserPermission.value, (user.value.role || 'user')))
+const featureMenus = ref(getSidebarMenus(OPS_AND_MANAGEMENT_SIDEBAR_MENUS, currentUserPermission.value, (user.value.role || 'user')))
+const adminMenus = ref(getSidebarMenus(ADMIN_SIDEBAR_MENUS.value, currentUserPermission.value, (user.value.role || 'user')))
 
 const SidebarData = {
   logoIcon,
