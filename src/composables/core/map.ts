2063e15ee7986f146bc489cc649cec49711ad693
kromate <akpananthony33@gmail.com>
2023-12-21 12:25:05 +0100

    update

--- src/composables/core/map.ts
+++ src/composables/core/map.ts
@@ -3,9 +3,8 @@ import { Loader } from '@googlemaps/js-api-loader'
 
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string
 export const loader = new Loader({
-    apiKey: GOOGLE_MAPS_API_KEY,
-    libraries: ['places', 'marker', 'drawing'],
-    version: 'beta'
+	apiKey: GOOGLE_MAPS_API_KEY as string,
+	libraries: ['places']
 })
 
 export const InitedLoader = Loader
