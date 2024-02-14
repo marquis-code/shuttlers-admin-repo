7447d9a8569a857f433da3de2e91d3c920893dbd
kromate <akpananthony33@gmail.com>
2024-02-14 13:22:09 +0100

    completed the sales agent ui

--- src/components/modals/modules/Routes/ConfigureAgentCommission.vue
+++ src/components/modals/modules/Routes/ConfigureAgentCommission.vue
@@ -4,15 +4,19 @@
 		title="Configure commission"
 	>
 		<form class="flex flex-col gap-6" @submit.prevent="notify">
+			<div class="flex justify-between items-center bg-neut1 text-black input-field">
+				<span>Current commission</span>
+				<span>N/A</span>
+			</div>
 			<div class="field relative">
 				<label for="Reason">Commission (%) </label>
-				<TagSelect v-model="sos_data.sos_request_ids.value" :options="selectedSosTrip?.requests" display-key="sos_reason" placeholder="Select reason" />
+				<input id="First" v-model="sos_data.sos_request_ids.value" type="number" class="input-field" placeholder="Enter value" required>
 			</div>
 
 			<div class="flex">
 				<button class="btn-primary w-full" :disabled="loading || sos_data.sos_request_ids.value.length === 0">
 					<span v-if="!loading" class="flex justify-center items-center gap-2.5">
-						Notify
+						Configure
 					</span>
 					<Spinner v-else />
 				</button>
