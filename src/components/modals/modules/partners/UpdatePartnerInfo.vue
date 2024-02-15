0eacbe2bb76d06664ed77c33693ee3de4cbf575a
unknown <saykeed@gmail.com>
2024-02-15 12:44:05 +0100

    update on partners

---
+++ src/components/modals/modules/partners/UpdatePartnerInfo.vue
@@ -0,0 +1,40 @@
+<template>
+	<Modal
+		modal="$atts.modal"
+		title="Update Partner"
+	>
+		<form class="flex flex-col gap-[16px] w-full" @submit.prevent="updatePartner">
+			<div class="flex flex-col gap-2">
+				<label class="label">First name</label>
+				<input v-model="fname" required type="text" class="input-field">
+			</div>
+			<div class="flex flex-col gap-2">
+				<label class="label">Last name</label>
+				<input v-model="lname" required type="text" class="input-field">
+			</div>
+			<div class="flex flex-col gap-2">
+				<label class="label">Email</label>
+				<input v-model="email" disabled required type="email" class="input-field">
+			</div>
+			<button type="submit" :disabled="loading || !enableButton" class="text-sm bg-black p-[16px] text-white text-center w-full mt-2 rounded disabled:cursor-not-allowed disabled:bg-[#E0E6ED]">
+				{{ loading ? 'processing...' : 'Update Partner' }}
+			</button>
+		</form>
+	</Modal>
+</template>
+
+<script setup lang="ts">
+import { useUpdatePartnerInfo } from '@/composables/modules/partners'
+
+const { loading, clearObj, fname, lname, email, updatePartner } = useUpdatePartnerInfo()
+const enableButton = computed(() => {
+	return !!(fname.value && lname.value && email.value)
+})
+onBeforeUnmount(() => clearObj())
+</script>
+
+<style scoped>
+label{
+	margin: 0;
+}
+</style>
