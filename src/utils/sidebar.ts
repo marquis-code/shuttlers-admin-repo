67bf85866642747b96d3c1145f66105ee29c9ab8
kromate <akpananthony33@gmail.com>
2024-02-26 16:57:17 +0100

    remove sales agent form prod

--- src/utils/sidebar.ts
+++ src/utils/sidebar.ts
@@ -107,13 +107,13 @@ export const OPS_AND_MANAGEMENT_SIDEBAR_MENUS = [
       }
     ]
   },
-  {
-  title: 'Sales agents',
-    routePath: '/agents',
-    description: '',
-    iconComponent: shallowRef(userIcon),
-    rootPath: '/agents'
-  },
+  // {
+  // title: 'Sales agents',
+  //   routePath: '/agents',
+  //   description: '',
+  //   iconComponent: shallowRef(userIcon),
+  //   rootPath: '/agents'
+  // },
   {
     title: 'Commute',
     routePath: '/trips/type/active',
