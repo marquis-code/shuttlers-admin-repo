21148027254124e4924ef078ed24c6ba5a39002e
kromate <akpananthony33@gmail.com>
2024-02-26 12:03:57 +0100

    update on logout

--- src/api_factory/modules/agent.ts
+++ src/api_factory/modules/agent.ts
@@ -4,7 +4,7 @@ import { TMetaObject, useTableFilter } from '@/composables/utils/table'
 export const agents_api = {
 	$_get_agents: (meta:TMetaObject, filterData?: Record<string, Ref>) => {
 		const queryParams = useTableFilter(filterData)
-		const url = `/sales-agent?${queryParams}&limit=${meta.page_size.value}&page=${meta.page.value}&metadata=true`
+		const url = `/users?account_type=sales_agent&${queryParams}&limit=${meta.page_size.value}&page=${meta.page.value}&metadata=true`
 		return GATEWAY_ENDPOINT_WITH_AUTH.get(url)
 	},
     $_create_batch_booking: (payload: any) => {
