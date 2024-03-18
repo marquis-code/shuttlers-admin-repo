c77006ea5d46381dfaab0b4fc544bf50ecc4319e
Marquis Abah <marquis.abah@shuttlers.ng>
2024-03-18 11:18:04 +0100

    fix page refresh after new staff has been added to corporate group

--- src/components/modals/modules/companies/BatchAddCorporateGroupMembers.vue
+++ src/components/modals/modules/companies/BatchAddCorporateGroupMembers.vue
@@ -1,17 +1,15 @@
 <template>
-	<Modal
-		modal="$atts.modal"
-		title="Add Members"
-	>
+	<Modal modal="$atts.modal" title="Add Members">
 		<form class="flex flex-col gap-6 space-y-7" @submit.prevent="handleSubmit">
 			<div class="field relative">
 				<CorporatesStaffMultiSelect class="w-full" label="" @update:modelValue="handleSelectedCorporates" />
 			</div>
 			<div class="flex space-x-6">
-				<button class="bg-gray-500 text-white p-3 rounded-md w-full" @click="useCompaniesModal().closeBatchAddCorporateGroupMembers()">
+				<button type="button" class="bg-gray-500 text-white p-3 rounded-md w-full"
+					@click="useCompaniesModal().closeBatchAddCorporateGroupMembers()">
 					Cancel
 				</button>
-				<button class="btn-primary w-full" :disabled="!isFormEmpty">
+				<button type="submit" class="btn-primary w-full" :disabled="!isFormEmpty">
 					<span v-if="!processing" class="flex justify-center items-center gap-2.5">
 						Add
 					</span>
@@ -25,18 +23,19 @@
 <script setup lang="ts">
 import { useAddCorporateGroupMembers } from '@/composables/modules/corporates/createCorporateGroupMembers'
 import { useCompaniesModal } from '@/composables/core/modals'
+import { useCorporateGroupByGroupId } from '@/composables/modules/corporates/getCorporateGroupByGroupId'
 const { createCorporateGroupMembers, loading: processing, payload } = useAddCorporateGroupMembers()
-
-const handleSelectedCorporates = (val:any) => {
+const { getCorporateGroupByGroupId } = useCorporateGroupByGroupId()
+const handleSelectedCorporates = (val: any) => {
 	payload.staff_ids.value.push(val.id)
 }
 
 const isFormEmpty = computed(() => {
-    return !!(payload.staff_ids.value)
+	return !!(payload.staff_ids.value)
 })
 
-const handleSubmit = () => {
-	createCorporateGroupMembers()
+const handleSubmit = async () => {
+	await createCorporateGroupMembers()
 }
 
 </script>
