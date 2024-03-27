c7a4980baea9fd659f3a68b5f9e0a54c0aa85eff
kromate <akpananthony33@gmail.com>
2024-03-27 14:32:54 +0100

    UI update

--- src/pages/trips/routes/create.vue
+++ src/pages/trips/routes/create.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="flex">
 		<ModulesRoutesCreateForm class="md:min-w-[360px] w-4/12" />
-
 		<MapDisplay :start-point="startPosition" :end-point="endPosition" :props-loading="ploylineLoading" :encoded-polyline="polyline" class="w-8/12 rounded-br-xl" height="87vh" />
 	</div>
 </template>
