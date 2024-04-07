36c08c72f7856c181fff9bae71b5c694385a4475
kromate <akpananthony33@gmail.com>
2024-04-07 23:33:28 +0100

    update swap config

--- src/components/modals/modules/agents/ConfigureAgentPoints.vue
+++ src/components/modals/modules/agents/ConfigureAgentPoints.vue
@@ -8,15 +8,18 @@
 		<form class="flex flex-col items-center justify-center" @submit.prevent="confirm">
 			<div class="field relative">
 				<label for="amount" class="text-[12px] text-[#6E717C] font-[500] leading-[20px]">Naira equivalent</label>
-				<input id="amount" v-model="rate" :placeholder="currenciesLoading? 'loading...' : '₦'" :disabled="currenciesLoading" type="number" class="input-field" required>
+				<input id="amount" v-model="rate" :placeholder="(currenciesLoading || rateLoading)? 'loading...' : '₦'" :disabled="currenciesLoading || rateLoading" type="number" class="input-field" required>
 			</div>
 			<div class="bg-[#F4F5F4] flex justify-between items-center py-3 rounded-lg px-[12px] w-full mt-3">
 				<p class="text-[#6E717C] font-extralight leading-[21px] text-[14px]">
 					1 point
 				</p>
 				<p>~</p>
-				<p class="text-[#6E717C] font-extralight leading-[21px] text-[14px]">
+				<p v-if="!rateLoading" class="text-[#6E717C] font-extralight leading-[21px] text-[14px]">
 					{{ convertToCurrency(Number(rate)) }}
+				</p>
+				<p v-else class="text-[#6E717C] font-extralight leading-[21px] text-[14px]">
+					loading...
 				</p>
 			</div>
 			<button :disabled="!rate" class="btn-primary mt-12 w-full">
@@ -30,12 +33,23 @@
 <script setup lang="ts">
 
 import { convertToCurrency } from '@/composables/utils/formatter'
-import { useGetCurrencies, useCreateSwapConfiguration } from '@/composables/modules/agents/configure'
+import { useGetCurrencies, useCreateSwapConfiguration, useGetCurrentRatesForAgent } from '@/composables/modules/agents/configure'
 
 const { getCurrencies, loading: currenciesLoading } = useGetCurrencies()
 const { loading, confirm, rate } = useCreateSwapConfiguration()
+const { rateData, rateLoading, getRateForAgent } = useGetCurrentRatesForAgent()
 
 getCurrencies()
+getRateForAgent()
+
+watch(rateData, (val:any) => {
+	console.log(val)
+	if (val.rate) {
+		rate.value = val.rate
+	} else {
+		rate.value = 0
+	}
+})
 
 </script>
 
