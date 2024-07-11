bbb441b1838f7fd364eaef87a83b79c5cfac98e8
saykeed <sherif.abubakri@shuttlers.ng>
2024-07-11 10:32:24 +0100

    updae on price bound table

--- src/composables/modules/routes/itineraries/fetch.ts
+++ src/composables/modules/routes/itineraries/fetch.ts
@@ -7,6 +7,7 @@ const loading = ref(false)
 const loading_single_iti = ref(false)
 export const singleItinerary = ref({}) as Ref<Record<string, any>>
 const returnTripItinerary = ref({}) as Ref<Record<string, any>>
+const lookupTablePrices = ref([] as Record<string, any>[])
 
 export const useItineraries = () => {
 	const getItineraries = async () => {
@@ -33,10 +34,30 @@ export const useItineraries = () => {
 		if (res.type !== 'ERROR') {
 			singleItinerary.value = res.data?.id ? res.data : {}
 			if (singleItinerary.value?.itinerary_pair_id) getReturnTripItinerary(singleItinerary.value.itinerary_pair_id)
+				lookupTablePrices.value = singleItinerary.value?.prices?.length ? singleItinerary.value?.prices : []
 			resetObj()
 		}
 		loading_single_iti.value = false
 	}
 
-	return { loading, itineraries, getItineraries, loading_single_iti, singleItinerary, getSingleItinerary, returnTripItinerary }
+	const updatePriceBound = async () => {
+		const payload = {
+			create: lookupTablePrices.value.map((el) => {
+				return {
+					start_route_bus_stop_id: el?.start_route_bus_stop_id,
+					end_route_bus_stop_id: el?.end_route_bus_stop_id,
+					fare: el?.fare,
+					fare_currency: 'NGN'
+				}
+			})
+		}
+		console.log(payload)
+		const itinerary_id = useRoute().params.iti_id as string
+		const res = await routes_api.$_update_itinerary_price_bound(Number(itinerary_id), payload) as CustomAxiosResponse
+		if (res.type !== 'ERROR') {
+			console.log(res)
+		}
+	}
+
+	return { loading, itineraries, getItineraries, loading_single_iti, singleItinerary, getSingleItinerary, returnTripItinerary, lookupTablePrices, updatePriceBound }
 }
