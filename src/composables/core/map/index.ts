fa2dc554a8ca603e1ab5dac891bcd48abeb42d7c
kromate <akpananthony33@gmail.com>
2024-07-29 23:42:24 +0100

    map changes

--- src/composables/core/map/index.ts
+++ src/composables/core/map/index.ts
@@ -1,19 +1,20 @@
+import { ref, Ref } from 'vue'
 import { LatLng, MapService } from './interface/index'
-import { Coordinate } from './types'
+import { Coordinate, UserCoordinate } from './types'
 import { ShouldUseGoogleMaps } from '@/composables/flagging/flags'
 
 export let map
-const mapType = ShouldUseGoogleMaps() ? 'google' : 'osm'
-export const mapService = new MapService(mapType)
+const defaultMapType = ShouldUseGoogleMaps() ? 'google' : 'osm'
+export const mapService = new MapService(defaultMapType)
 
 export const loading = ref(false)
 
 export const calculateCenterAndZoom = async (coord1: Coordinate, coord2: Coordinate) => {
-    mapService.calculateCenterAndZoom(coord1, coord2)
+    await mapService.calculateCenterAndZoom(coord1, coord2)
 }
 
-export const initMap = async (mapDiv: Ref, mapId: string | null = null) => {
-mapService.initMap(mapDiv, mapId)
+export const initMap = async (mapDiv: Ref, mapId: string | null = null, mapType?: 'google' | 'osm') => {
+    await mapService.initMap(mapDiv, mapId, mapType)
 }
 
 export const getPathFromPolyline = async (overviewPolyline) => {
@@ -25,13 +26,25 @@ export const getPathFromPolyline = async (overviewPolyline) => {
 }
 
 export const loadPolyline = async (pathLine: LatLng[]): Promise<void> => {
-    mapService.loadPolyline(pathLine)
+    await mapService.loadPolyline(pathLine)
 }
 
 export const loadBusstops = async (stops: Record<string, any>[]): Promise<void> => {
-  mapService.loadBusstops(stops)
+    await mapService.loadBusstops(stops)
 }
 
 export const addPointOnMap = async (location: Coordinate) => {
-mapService.addPointOnMap(location)
+    await mapService.addPointOnMap(location)
+}
+
+export const loadMarkerOnMap = async (location: UserCoordinate, clickFunc: (location: Coordinate) => void, imgString?: string, direction?: number): Promise<void> => {
+    await mapService.loadMarkerOnMap(location, clickFunc, imgString, direction)
+}
+
+export const zoomMapInOnCoordinate = async (location: Coordinate): Promise<void> => {
+    await mapService.zoomMapInOnCoordinate(location)
+}
+
+export const setActiveTrip = async (id: string): Promise<void> => {
+    await mapService.setActiveTrip(id)
 }
--- src/composables/core/map/interface/index.ts
+++ src/composables/core/map/interface/index.ts
@@ -7,26 +7,21 @@ export interface LatLng {
   lng: number;
 }
 
-// map.interface.ts
-export interface MapProvider {
-  initMap(mapDiv: Ref<HTMLElement | null>, mapId?: string | null): Promise<void>;
-  calculateCenterAndZoom(coord1: Coordinate, coord2: Coordinate): Promise<void>;
-  loadPolyline(pathLine: LatLng[]): Promise<void>;
-  loadBusstops(stops: Record<string, any>[]): Promise<void>;
-  addPointOnMap(location: Coordinate): Promise<void>;
-  loadMarkerOnMap(location: UserCoordinate, clickFunc: (location: UserCoordinate) => void, imgString?: string, direction?: number): Promise<void>;
-  zoomMapInOnCoordinate(location: Coordinate): Promise<void>;
-  setActiveTrip(id: string): Promise<void>;
-}
-
 export class MapService {
   private mapProvider: MapProvider
 
-  constructor(mapProviderType: 'osm' | 'google') {
-    this.mapProvider = mapProviderType === 'osm' ? new OsmMap() : new GoogleMaps()
+  constructor(private defaultMapProviderType: 'osm' | 'google') {
+    this.mapProvider = this.createMapProvider(defaultMapProviderType)
   }
 
-  async initMap(mapDiv: Ref<HTMLElement | null>, mapId?: string | null): Promise<void> {
+  private createMapProvider(mapProviderType: 'osm' | 'google'): MapProvider {
+    return mapProviderType === 'osm' ? new OsmMap() : new GoogleMaps()
+  }
+
+  async initMap(mapDiv: Ref<HTMLElement | null>, mapId?: string | null, mapProviderType?: 'osm' | 'google'): Promise<void> {
+    if (mapProviderType && mapProviderType !== this.defaultMapProviderType) {
+      this.mapProvider = this.createMapProvider(mapProviderType)
+    }
     await this.mapProvider.initMap(mapDiv, mapId)
   }
 
@@ -57,4 +52,16 @@ export class MapService {
   async setActiveTrip(id: string): Promise<void> {
     await this.mapProvider.setActiveTrip(id)
   }
+}
+
+// MapProvider interface
+export interface MapProvider {
+  initMap(mapDiv: Ref<HTMLElement | null>, mapId?: string | null): Promise<void>;
+  calculateCenterAndZoom(coord1: Coordinate, coord2: Coordinate): Promise<void>;
+  loadPolyline(pathLine: LatLng[]): Promise<void>;
+  loadBusstops(stops: Record<string, any>[]): Promise<void>;
+  addPointOnMap(location: Coordinate): Promise<void>;
+  loadMarkerOnMap(location: UserCoordinate, clickFunc: (location: UserCoordinate) => void, imgString?: string, direction?: number): Promise<void>;
+  zoomMapInOnCoordinate(location: Coordinate): Promise<void>;
+  setActiveTrip(id: string): Promise<void>;
 }
